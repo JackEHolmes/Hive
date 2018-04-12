8d66773881dc20f4fed47c8c5155fab67fa63afc
JackEHolmes <348806836@qq.com>
2018-04-13 02:40:53 +0800

    Update jdk-hadoop-hive.sh

--- jdk-hadoop-hive.sh
+++ jdk-hadoop-hive.sh
@@ -1,28 +1,5 @@
 #jdk-1.8.0_162
-echo "export JAVA_HOME=/usr/java/jdk-1.8.0" > java.sh
-echo "export JRE_HOME=\${JAVA_HOME}/jre" >> java.sh
-echo "export CLASSPATH=.:\${JAVA_HOME}/lib:\${JRE_HOME}/lib" >> java.sh
-echo "export PATH=\${JAVA_HOME}/bin:\$PATH" >> java.sh
-sudo mv java.sh /etc/profile.d/
-
-echo "export HIVE_HOME=/usr/hive/hive-1.2.2" >hive.sh
-echo "export PATH=\${HIVE_HOME}/bin:\$PATH" >> hive.sh
-echo "export HIVE_CONF_DIR=\${HIVE_HOME}/conf" >>hive.sh
-sudo mv hive.sh /etc/profile.d/
-
-echo "export HADOOP_HOME=/usr/hadoop/hadoop-2.9.0" > hadoop.sh
-echo "export PATH=\${HADOOP_HOME}/bin:\${HADOOP_HOME}/sbin:\$PATH" >> hadoop.sh
-echo "export HADOOP_CLASSPATH=.:\$CLASSPATH:\$HADOOP_CLASSPATH:\$HADOOP_HOME/lib:\$HADOOP_HOME/bin" >> hadoop.sh
-sudo mv hadoop.sh /etc/profile.d/
-
-sudo chmod u+x /etc/profile.d/java.sh
-sudo chmod u+x /etc/profile.d/hadoop.sh
-sudo chmod u+x /etc/profile.d/hive.sh
-
-sudo /etc/profile.d/java.sh
-sudo /etc/profile.d/hadoop.sh
-sudo /etc/profile.d/hive.sh
-
+export JAVA_HOME=/usr/java/jdk-1.8.0
 mv jdk-8u*tar.gz* jdk-1.8.0.tar.gz
 if [ -f "jdk-1.8.0.tar.gz" ]; then
 echo "jdk-*.tar.gz  exists"
@@ -43,6 +20,7 @@ sudo chown $USER -R /usr/java
 sudo chgrp $USER -R /usr/java
 
 #hadoop-2.9.0
+export HIVE_HOME=/usr/hive/hive-1.2.2
 if [ -f "hadoop-2.9.0.tar.gz" ]; then 
 echo "hadoop-2.9.0.tar.gz  exists"
 else
@@ -60,6 +38,7 @@ sudo chown $USER -R /usr/hadoop
 sudo chgrp $USER -R /usr/hadoop
 
 #hive-1.2.2
+export HIVE_HOME=/usr/hive/hive-1.2.2
 if [ -f "apache-hive-1.2.2-bin.tar.gz" ]; then 
 echo "apache-hive-1.2.2-bin.tar.gz  exists"
 else
