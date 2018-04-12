1e7e3ca3bebed91d869c0508a9470312717582e6
JackEHolmes <348806836@qq.com>
2018-04-12 21:32:37 +0800

    Update local_mysql.sh

--- local_mysql/server/local_mysql.sh
+++ local_mysql/server/local_mysql.sh
@@ -3,5 +3,4 @@ wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.46
 tar -zxvf mysql-connector-java-5.1.46.tar.gz
 cp mysql-connector-java-5.1.46/mysql*.jar $HIVE_HOME/lib
 rm -rf mysql-connector-java-5.1.46
-wget https://raw.githubusercontent.com/JackEHolmes/Hive/master/Local_mysql/hive-site.xml -P $HIVE_HOME/conf/
-
+wget https://raw.githubusercontent.com/JackEHolmes/Hive_install/master/local_mysql/server/hive-site.xml -P $HIVE_HOME/conf/
