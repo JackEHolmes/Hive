3f09380bdef0a6487b3f05d3e76fa2f425855ec7
JackEHolmes <348806836@qq.com>
2018-04-13 10:24:00 +0800

    Create hive.sql

---
+++ local_mysql/server/hive.sql
@@ -0,0 +1,9 @@
+create database hive;  
+create user hive@localhost identified by 'Hive_12345';  
+grant all on hive.* to hive@localhost;  
+  
+  
+  ***
+  
+revoke all from hive@localhos  
+drop user hive@localhost;  
