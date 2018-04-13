8966697e8638813a2d2a20a0c04d220118d4166e
JackEHolmes <348806836@qq.com>
2018-04-13 10:24:40 +0800

    Update hive.sql

--- local_mysql/server/hive.sql
+++ local_mysql/server/hive.sql
@@ -2,8 +2,6 @@ create database hive;
 create user hive@localhost identified by 'Hive_12345';  
 grant all on hive.* to hive@localhost;  
   
-  
-  ***
-  
-revoke all from hive@localhos  
-drop user hive@localhost;  
+
+#revoke all from hive@localhos  
+#drop user hive@localhost;  
