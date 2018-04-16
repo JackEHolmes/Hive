7f1441cfe489e9af77075e6aa4a3e26117f2919f
JackEHolmes <348806836@qq.com>
2018-04-16 17:13:44 +0800

    Update local_mysql_server.sh

--- Pseudo distributed/local_mysql_server.sh
+++ Pseudo distributed/local_mysql_server.sh
@@ -15,8 +15,13 @@ fi
 tar -zxvf mysql-connector-java-5.1.46.tar.gz
 cp mysql-connector-java-5.1.46/mysql*-bin.jar $HIVE_HOME/lib
 rm -rf mysql-connector-java-5.1.46
-wget https://raw.githubusercontent.com/JackEHolmes/Hive_install/master/local_mysql/server/hive-site.xml -P $HIVE_HOME/conf/
+wget https://github.com/JackEHolmes/hive_install/raw/master/Pseudo%20distributed/hive-site.xml -P $HIVE_HOME/conf/
 cp $HIVE_HOME/conf/hive-default.xml.template $HIVE_HOME/conf/hive-default.xml
 cp $HIVE_HOME/conf/hive-env.sh.template $HIVE_HOME/conf/hive-env.sh
 ln -s /usr/hadoop/hadoop-2.9.0/sbin/start-dfs.sh /etc/rc3.d/S07start-dfs.sh
 ln -s /usr/hadoop/hadoop-2.9.0/sbin/start-yarn.sh /etc/rc3.d/S07start-yarn.sh
+wget https://github.com/JackEHolmes/hive_install/raw/master/Pseudo%20distributed/core-site.xml -P $HADOOP_HOME/etc/hadoop/
+wget https://github.com/JackEHolmes/hive_install/raw/master/Pseudo%20distributed/hdfs-site.xml -P $HADOOP_HOME/etc/hadoop/
+wget https://github.com/JackEHolmes/hive_install/raw/master/Pseudo%20distributed/mapred-site.xml -P $HADOOP_HOME/etc/hadoop/
+wget https://github.com/JackEHolmes/hive_install/raw/master/Pseudo%20distributed/yarn-site.xml -P $HADOOP_HOME/etc/hadoop/
+
