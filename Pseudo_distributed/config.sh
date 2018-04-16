920d6a613d807325059d78af50a3982d469d23f0
JackEHolmes <348806836@qq.com>
2018-04-16 20:28:27 +0800

    Update config.sh

--- Pseudo_distributed/config.sh
+++ Pseudo_distributed/config.sh
@@ -15,9 +15,14 @@ fi
 tar -zxvf mysql-connector-java-5.1.46.tar.gz
 sudo cp mysql-connector-java-5.1.46/mysql*-bin.jar $HIVE_HOME/lib
 rm -rf mysql-connector-java-5.1.46
+sudo rm $HIVE_HOME/conf/hive-site.xml
 sudo wget https://github.com/JackEHolmes/hive_install/raw/master/Pseudo_distributed/hive-site.xml -P $HIVE_HOME/conf/
 sudo cp $HIVE_HOME/conf/hive-default.xml.template $HIVE_HOME/conf/hive-default.xml
 sudo cp $HIVE_HOME/conf/hive-env.sh.template $HIVE_HOME/conf/hive-env.sh
+sudo rm $HADOOP_HOME/etc/hadoop/core-site.xml
+sudo rm $HADOOP_HOME/etc/hadoop/hdfs-site.xml
+sudo rm $HADOOP_HOME/etc/hadoop/mapred-site.xml
+sudo rm $HADOOP_HOME/etc/hadoop/yarn-site.xml
 sudo wget https://github.com/JackEHolmes/hive_install/raw/master/Pseudo_distributed/core-site.xml -P $HADOOP_HOME/etc/hadoop/
 sudo wget https://github.com/JackEHolmes/hive_install/raw/master/Pseudo_distributed/hdfs-site.xml -P $HADOOP_HOME/etc/hadoop/
 sudo wget https://github.com/JackEHolmes/hive_install/raw/master/Pseudo_distributed/mapred-site.xml -P $HADOOP_HOME/etc/hadoop/
