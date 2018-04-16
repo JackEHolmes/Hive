bc5908802ee1afef06fe9fa113e754c2383e0eda
JackEHolmes <348806836@qq.com>
2018-04-16 11:42:54 +0800

    Update wctest.sh

--- wctest.sh
+++ wctest.sh
@@ -4,3 +4,5 @@ hdfs dfs -mkdir /wc
 hdfs dfs -put input.txt /wc
 hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.9.0.jar wordcount /wc/input.txt /wc/ouput
 hdfs dfs -cat /wc/ouput/*
+
+hdfs dfs -rm -r /wc
