a30073428fa74458f79771fc9f4ddcc56eb786ac
JackEHolmes <348806836@qq.com>
2018-04-16 11:34:06 +0800

    wordcount

---
+++ wctest.sh
@@ -0,0 +1,4 @@
+echo -e 'hadoop mapreduce hive\nhbase spark storm\nsqoop hadoop hive\nspark hadoop'>input.txt
+hdfs dfs -mkdir /wc
+hdfs dfs -put input.txt /wc
+hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.9.0.jar wordcount /wc/input.txt /wc/ouput
