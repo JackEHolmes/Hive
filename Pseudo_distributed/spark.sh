12fced2d32ff13c36b3a9eed98fb2cf05ed0ec48
JackEHolmes <348806836@qq.com>
2018-05-15 22:27:23 +0800

    Update spark.sh

--- Pseudo_distributed/spark.sh
+++ Pseudo_distributed/spark.sh
@@ -1,3 +1,4 @@
 export SPARK_HOME=/usr/spark/spark-2.3.0-bin-hadoop2.7
 export PATH=$SPARK_HOME/bin:$SPARK_HOME/sbin:$PATH
-export CLASSPATH=.:$CLASSPATH:$SPARK_HOME/jar
+export CLASSPATH=.:$CLASSPATH:$SPARK_HOME/jars
+export SPARK_CLASSPATH=.:$CLASSPATH:$HADOOP_CLASSPATH:$HIVE_HOME/lib:$SPARK_HOME/jars
