a15a477734f656e554511041064519329795353f
JackEHolmes <348806836@qq.com>
2018-05-15 18:42:34 +0800

    Update spark.sh

--- Pseudo_distributed/spark.sh
+++ Pseudo_distributed/spark.sh
@@ -1,3 +1,3 @@
 export SPARK_HOME=/usr/spark/spark-2.3.0-bin-hadoop2.7
-export PATH=$SPARK_HOME/bin:$PATH
+export PATH=$SPARK_HOME/bin:$SPARK_HOME/sbin:$PATH
 export CLASSPATH=.:$CLASSPATH:$SPARK_HOME/jar
